2293374ae11c4050979f4efca23554c23469f78e
Santiago Uribe <santyur19@gmail.com>
2023-03-16 01:48:04 -0500

    casi terminado, solo falta información final

---
+++ code/src/temas/g_col/g_col.js
@@ -0,0 +1,10 @@
+function mensaje() {
+  Swal.fire({
+    title: "Frente Nacional",
+    text: "el frente nacional tal",
+    imageUrl: "/sociales/code/img/icon.png",
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: "Custom image",
+  });
+}
